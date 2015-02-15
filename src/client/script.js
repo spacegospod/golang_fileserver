778b8068f3ba8f4f3715fac460de5f15d47343bb
spacegospod <stoyozh@gmail.com>
2015-02-15 17:26:50 +0200

    Folders implementation

--- src/client/script.js
+++ src/client/script.js
@@ -22,6 +22,22 @@ window.onload = function() {
 		req.send(formData);
 	});
 	
+	var mkdirButton = document.getElementById('mkdirButton');
+	mkdirButton.addEventListener("click", function(e) {
+		var dirname = document.getElementById('dirname').value;
+		
+		var req = new XMLHttpRequest();
+		req.open('post', "/api/post/createdir/"+USER_NAME, true);
+		req.onreadystatechange = function() {
+			if (req.readyState == 4) {
+				var dirInfo = JSON.parse(req.responseText);
+				updateTable(dirInfo);
+			}
+		}
+
+		req.send(dirname);
+	});
+	
 	var loginButton = document.getElementById('loginButton');
 	loginButton.addEventListener("click", function(e) {	
 			username = document.getElementById('username').value;
@@ -35,9 +51,7 @@ window.onload = function() {
 						alert(resp[1]);
 					} else if (resp[0] == "success") {
 						var dirInfo = JSON.parse(resp[1]);
-						if (dirInfo.Files != null) {
-							updateTable(dirInfo);
-						}
+						updateTable(dirInfo);
 						USER_NAME = username;
 					}
 	   			}
@@ -72,9 +86,34 @@ window.onload = function() {
 	}
 		
 	function deleteHandler(e) {
-		var fileName = e.target.parentNode.parentNode.childNodes[0].childNodes[0].wholeText;
+		var itemName = e.target.parentNode.parentNode.childNodes[0].childNodes[0].wholeText;
 		var req = new XMLHttpRequest();
-        req.open("delete", "/api/delete/"+fileName, true);
+        req.open("delete", "/api/delete/"+USER_NAME+"&"+itemName, true);
+		req.onreadystatechange = function() {
+			if (req.readyState == 4) {
+				var dirInfo = JSON.parse(req.responseText);
+				updateTable(dirInfo);
+			}
+		}
+		req.send();
+	}
+	
+	function openFolderHandler(e) {
+		var folderName = e.target.parentNode.parentNode.childNodes[0].childNodes[0].wholeText;
+		var req = new XMLHttpRequest();
+        req.open("post", "/api/post/navigation/fwd&"+USER_NAME+"&"+folderName, true);
+		req.onreadystatechange = function() {
+			if (req.readyState == 4) {
+				var dirInfo = JSON.parse(req.responseText);
+				updateTable(dirInfo);
+			}
+		}
+		req.send();
+	}
+	
+	function navigationBackHandler(e) {
+		var req = new XMLHttpRequest();
+        req.open("post", "/api/post/navigation/back&"+USER_NAME+"&", true);
 		req.onreadystatechange = function() {
 			if (req.readyState == 4) {
 				var dirInfo = JSON.parse(req.responseText);
@@ -96,44 +135,83 @@ window.onload = function() {
 		table.border = "1"
 		
 		var headerRow = document.createElement('tr');
-		var filenameHeader = document.createElement('th');
-		filenameHeader.appendChild(document.createTextNode("Filename"));
-		var filesizeHeader = document.createElement('th');
-		filesizeHeader.appendChild(document.createTextNode("Size(kb)"));
-		headerRow.appendChild(filenameHeader);
-		headerRow.appendChild(filesizeHeader);
+		var nameHeader = document.createElement('th');
+		var backButton = document.createElement('img');
+		backButton.src = "back_button.png";
+		backButton.addEventListener("click", navigationBackHandler)
+		nameHeader.appendChild(backButton);
+		nameHeader.appendChild(document.createTextNode("Name"));
+		var sizeHeader = document.createElement('th');
+		sizeHeader.appendChild(document.createTextNode("Size(kb)"));
+		headerRow.appendChild(nameHeader);
+		headerRow.appendChild(sizeHeader);
 		table.appendChild(headerRow);
 		tableDiv.appendChild(table);
 		
-		dirInfo.Files.forEach(function(fileInfo) {
-			var row = document.createElement('tr');
-			
-			var fileNameColumn = document.createElement('td');
-			var fileName = document.createTextNode(fileInfo.Name);
-			fileNameColumn.appendChild(fileName);
-			
-			var fileSizeColumn = document.createElement('td');
-			var fileSize = document.createTextNode(Math.floor(fileInfo.Size / 1000));
-			fileSizeColumn.appendChild(fileSize);
-			
-			var downloadColumn = document.createElement('td');
-			var downloadButton = document.createElement('img');
-			downloadButton.src = "download_button.png";
-			downloadColumn.appendChild(downloadButton);
-			downloadButton.addEventListener("click", downloadHandler);
-			
-			var deleteColumn = document.createElement('td');
-			var deleteButton = document.createElement('img');
-			deleteButton.src = "delete_button.png";
-			deleteColumn.appendChild(deleteButton);
-			deleteButton.addEventListener("click", deleteHandler);
-			
-			row.appendChild(fileNameColumn);
-			row.appendChild(fileSizeColumn);
-			row.appendChild(downloadColumn);
-			row.appendChild(deleteColumn);
-			
-			table.appendChild(row);						
-		});
+		if (dirInfo.Files != undefined) {
+			dirInfo.Files.forEach(function(fileInfo) {
+				var row = document.createElement('tr');
+				
+				var fileNameColumn = document.createElement('td');
+				var fileName = document.createTextNode(fileInfo.Name);
+				fileNameColumn.appendChild(fileName);
+				
+				var fileSizeColumn = document.createElement('td');
+				var fileSize = document.createTextNode(Math.floor(fileInfo.Size / 1000));
+				fileSizeColumn.appendChild(fileSize);
+				
+				var downloadColumn = document.createElement('td');
+				var downloadButton = document.createElement('img');
+				downloadButton.src = "download_button.png";
+				downloadButton.addEventListener("click", downloadHandler);
+				downloadColumn.appendChild(downloadButton);
+				
+				var deleteColumn = document.createElement('td');
+				var deleteButton = document.createElement('img');
+				deleteButton.src = "delete_button.png";
+				deleteButton.addEventListener("click", deleteHandler);
+				deleteColumn.appendChild(deleteButton);
+				
+				row.appendChild(fileNameColumn);
+				row.appendChild(fileSizeColumn);
+				row.appendChild(downloadColumn);
+				row.appendChild(deleteColumn);
+				
+				table.appendChild(row);						
+			});
+		}
+
+		if (dirInfo.Folders != undefined) {
+			dirInfo.Folders.forEach(function(folderInfo) {
+				var row = document.createElement('tr');
+				
+				var folderNameColumn = document.createElement('td');
+				var folderName = document.createTextNode(folderInfo.Name);
+				folderNameColumn.appendChild(folderName);
+				
+				var folderSizeColumn = document.createElement('td');
+				var folderSize = document.createTextNode("--");
+				folderSizeColumn.appendChild(folderSize);
+				
+				var openColumn = document.createElement('td');
+				var openButton = document.createElement('img');
+				openButton.src = "folder_button.png";
+				openButton.addEventListener("click", openFolderHandler);
+				openColumn.appendChild(openButton);
+				
+				var deleteColumn = document.createElement('td');
+				var deleteButton = document.createElement('img');
+				deleteButton.src = "delete_button.png";
+				deleteButton.addEventListener("click", deleteHandler);
+				deleteColumn.appendChild(deleteButton);
+				
+				row.appendChild(folderNameColumn);
+				row.appendChild(folderSizeColumn);
+				row.appendChild(openColumn);
+				row.appendChild(deleteColumn);
+				
+				table.appendChild(row);			
+			});
+		}
 	}
 };
--- src/fileserver/fileserver.go
+++ src/fileserver/fileserver.go
@@ -4,25 +4,27 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/bmizerany/pat"
 	"io"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
-	"pat"
 	"strconv"
 	"strings"
 )
 
 const (
-	API_PREFIX      = "/api/"
-	POST_PREFIX     = "post/"
-	DELETE_PREFIX   = "delete/"
-	DOWNLOAD_PREFIX = "download/"
-	UPLOAD_PREFIX   = "upload/"
-	LOGIN_PREFIX    = "login/"
-	SIGNUP_PREFIX   = "signup/"
+	API_PREFIX        = "/api/"
+	POST_PREFIX       = "post/"
+	DELETE_PREFIX     = "delete/"
+	DOWNLOAD_PREFIX   = "download/"
+	UPLOAD_PREFIX     = "upload/"
+	CREATE_DIR_PREFIX = "createdir/"
+	NAVIGATION_PREFIX = "navigation/"
+	LOGIN_PREFIX      = "login/"
+	SIGNUP_PREFIX     = "signup/"
 
 	USER_DIRECTORY_PREFIX = "user-"
 	PORT                  = 8080
@@ -30,25 +32,28 @@ const (
 
 var (
 	ROOT_DIR        string = "root"
+	NAVIGATION_FWD         = "fwd"
+	NAVIGATION_BACK        = "back"
 	USER_LOGIN_DATA        = make(map[string]bool)
 	USERS           []user
+	USER_DIRS       = make(map[string]string)
 )
 
 /* --- types --- */
 type userNotFoundError struct {
-	userName string
+	username string
 }
 
 func (err *userNotFoundError) Error() string {
-	return err.userName + " does not exist"
+	return err.username + " does not exist"
 }
 
 type userExistsError struct {
-	userName string
+	username string
 }
 
 func (err *userExistsError) Error() string {
-	return "User " + err.userName + " is already registered!"
+	return "User " + err.username + " is already registered!"
 }
 
 type user struct {
@@ -70,17 +75,23 @@ type fileInfo struct {
 	Size int64  `json:size`
 }
 
+type folderInfo struct {
+	Name string `json:name`
+}
+
 type dirInfo struct {
-	Files []fileInfo `json:files`
+	Files   []fileInfo   `json:files`
+	Folders []folderInfo `json:folders`
 }
 
 /* --- HTTP handlers --- */
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	parameters := strings.Split(strings.Replace(r.URL.String(), API_PREFIX+DOWNLOAD_PREFIX, "", 1), "&")
-	userName := strings.Split(parameters[0], "=")[1]
+	username := strings.Split(parameters[0], "=")[1]
 	fileName := parameters[1]
-	if hasAccess(userName) {
-		serveFileForDownload(w, r, fileName)
+	if hasAccess(username) {
+		filePath := USER_DIRS[username] + string(os.PathSeparator) + fileName
+		serveFileForDownload(w, r, filePath)
 	}
 	return
 }
@@ -100,20 +111,93 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Fprintln(w, err.Error())
 		} else {
-			dirInfo, e := readDirectory(ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username)
+			dirPath := USER_DIRS[username]
+			jsonDirInfo, e := generateDirectoryInfo(dirPath)
+
 			if e != nil {
 				fmt.Fprintf(w, e.Error())
 			} else {
-				jsonDirInfo, err := json.Marshal(dirInfo)
-				if err != nil {
-					log.Print(err.Error())
-				} else {
-					fmt.Fprintf(w, string(jsonDirInfo))
-				}
+				fmt.Fprintf(w, jsonDirInfo)
 			}
 		}
 	}
 	return
+}
+
+func createDirHandler(w http.ResponseWriter, r *http.Request) {
+	username := strings.Replace(r.URL.String(), API_PREFIX+POST_PREFIX+CREATE_DIR_PREFIX, "", 1)
+	if !hasAccess(username) {
+		return
+	}
+	dirNameBytes, _ := ioutil.ReadAll(r.Body)
+	dirName := string(dirNameBytes)
+
+	err := os.Mkdir(ROOT_DIR+string(os.PathSeparator)+USER_DIRECTORY_PREFIX+username+string(os.PathSeparator)+dirName, 0777)
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	} else {
+		currentPath := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username
+		jsonDirInfo, e := generateDirectoryInfo(currentPath)
+
+		if e != nil {
+			fmt.Fprintf(w, e.Error())
+		} else {
+			fmt.Fprintf(w, jsonDirInfo)
+		}
+	}
+
+	return
+}
+
+func navigationHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: check params for consistency
+	params := strings.Split(strings.Replace(r.URL.String(), API_PREFIX+POST_PREFIX+NAVIGATION_PREFIX, "", 1), "&")
+	direction := params[0]
+	username := params[1]
+
+	if !hasAccess(username) {
+		// TODO: err msg
+		return
+	}
+
+	if direction == NAVIGATION_BACK {
+		dirinfo, err := navigateBack(username)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		} else {
+			fmt.Fprintf(w, dirinfo)
+		}
+	} else if direction == NAVIGATION_FWD {
+		dirname := params[2]
+
+		dirinfo, err := openDir(username, dirname)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		} else {
+			fmt.Fprintf(w, dirinfo)
+		}
+	} else {
+		// something's wrong
+	}
+}
+
+func navigateBack(username string) (string, error) {
+	currentDirPath := USER_DIRS[username]
+	pathEntries := strings.Split(currentDirPath, string(os.PathSeparator))
+	newDirPath := strings.Replace(currentDirPath, string(os.PathSeparator)+pathEntries[len(pathEntries)-1], "", 1)
+
+	jsonDirInfo, e := generateDirectoryInfo(newDirPath)
+
+	return jsonDirInfo, e
+}
+
+func openDir(username string, dirname string) (string, error) {
+	dirPath := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username + string(os.PathSeparator) + dirname
+	USER_DIRS[username] = dirPath
+	jsonDirInfo, e := generateDirectoryInfo(dirPath)
+
+	return jsonDirInfo, e
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -129,17 +213,17 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if !loginUser(u) {
 		fmt.Fprintf(w, "failed&Invalid username or password")
 	} else {
-		dirInfo, e := readDirectory(ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + u.Username)
+		dirName := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + u.Username
+		jsonDirInfo, e := generateDirectoryInfo(dirName)
+
 		if e != nil {
-			fmt.Fprintf(w, "failed&"+e.Error())
+			fmt.Fprintf(w, e.Error())
 		} else {
-			jsonDirInfo, err := json.Marshal(dirInfo)
-			if err != nil {
-				log.Print(err.Error())
-			}
-			fmt.Fprintf(w, "success&"+string(jsonDirInfo))
+			fmt.Fprintf(w, "success&"+jsonDirInfo)
 		}
 	}
+
+	return
 }
 
 func signupHandler(w http.ResponseWriter, r *http.Request) {
@@ -157,32 +241,38 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 	os.Mkdir(ROOT_DIR+string(os.PathSeparator)+USER_DIRECTORY_PREFIX+newUser.Username, 0777)
+
+	return
 }
 
 func delHandler(w http.ResponseWriter, r *http.Request) {
-	fileName := strings.Replace(r.URL.String(), API_PREFIX+DELETE_PREFIX, "", 1)
-	err := os.Remove(ROOT_DIR + string(os.PathSeparator) + fileName)
+	params := strings.Split(strings.Replace(r.URL.String(), API_PREFIX+DELETE_PREFIX, "", 1), "&")
+	username := params[0]
+	itemName := params[1]
+
+	path := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username + string(os.PathSeparator) + itemName
+	err := os.RemoveAll(path)
+
 	if err != nil {
+		fmt.Println(err.Error())
 		fmt.Fprintf(w, err.Error())
 	} else {
-		dirInfo, e := readDirectory(ROOT_DIR)
+		dirName := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username
+		jsonDirInfo, e := generateDirectoryInfo(dirName)
+
 		if e != nil {
 			fmt.Fprintf(w, e.Error())
 		} else {
-			jsonDirInfo, err := json.Marshal(dirInfo)
-			if err != nil {
-				log.Print(err.Error())
-			} else {
-				fmt.Fprintf(w, string(jsonDirInfo))
-			}
+			fmt.Fprintf(w, jsonDirInfo)
 		}
 	}
+
 	return
 }
 
 func uploadFileToServer(file multipart.File, header *multipart.FileHeader, username string) (err error) {
-	userdir := ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + username
-	out, err := os.Create(userdir + string(os.PathSeparator) + header.Filename)
+	dir := USER_DIRS[username]
+	out, err := os.Create(dir + string(os.PathSeparator) + header.Filename)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -199,29 +289,52 @@ func uploadFileToServer(file multipart.File, header *multipart.FileHeader, usern
 	return nil
 }
 
-func serveFileForDownload(w http.ResponseWriter, r *http.Request, fileName string) {
-	http.ServeFile(w, r, ROOT_DIR+string(os.PathSeparator)+fileName)
+func serveFileForDownload(w http.ResponseWriter, r *http.Request, filepath string) {
+	http.ServeFile(w, r, filepath)
+	return
+}
+
+func generateDirectoryInfo(dirName string) (string, error) {
+	dirInfo, e := readDirectory(dirName)
+	if e != nil {
+		log.Print(e.Error())
+		return "", e
+	} else {
+		jsonDirInfo, err := json.Marshal(dirInfo)
+		if err != nil {
+			log.Print(err.Error())
+			return "", e
+		} else {
+			return string(jsonDirInfo), nil
+		}
+	}
 }
 
 func readDirectory(dirName string) (*dirInfo, error) {
-	files, err := ioutil.ReadDir(dirName)
+	items, err := ioutil.ReadDir(dirName)
 	var directoryInfo *dirInfo
 	if err != nil {
 		return directoryInfo, err
 	}
 
 	var fileInfos []fileInfo
-	for _, finfo := range files {
-		if !finfo.IsDir() {
+	var folderInfos []folderInfo
+	for _, item := range items {
+		if !item.IsDir() {
 			var fileInfo = new(fileInfo)
-			fileInfo.Name = finfo.Name()
-			fileInfo.Size = finfo.Size()
+			fileInfo.Name = item.Name()
+			fileInfo.Size = item.Size()
 			fileInfos = append(fileInfos, *fileInfo)
+		} else {
+			var folderInfo = new(folderInfo)
+			folderInfo.Name = item.Name()
+			folderInfos = append(folderInfos, *folderInfo)
 		}
 	}
 
 	directoryInfo = new(dirInfo)
 	directoryInfo.Files = fileInfos
+	directoryInfo.Folders = folderInfos
 
 	return directoryInfo, nil
 }
@@ -239,22 +352,23 @@ func loginUser(u user) bool {
 
 	if user.Password == u.Password {
 		USER_LOGIN_DATA[user.Username] = true
+		USER_DIRS[user.Username] = ROOT_DIR + string(os.PathSeparator) + USER_DIRECTORY_PREFIX + user.Username
 		return true
 	}
 
 	return false
 }
 
-func getUser(userName string) (user, error) {
+func getUser(username string) (user, error) {
 	var u user
 	for _, user := range USERS {
-		if user.Username == userName {
+		if user.Username == username {
 			u = user
 			return u, nil
 		}
 	}
 	var err = new(userNotFoundError)
-	err.userName = userName
+	err.username = username
 	return u, err
 }
 
@@ -262,7 +376,7 @@ func signupUser(newUser user) error {
 	_, e := getUser(newUser.Username)
 	if e == nil {
 		var err = new(userExistsError)
-		err.userName = newUser.Username
+		err.username = newUser.Username
 		return err
 	}
 	configFile := loadConfiguration()
@@ -318,6 +432,8 @@ func initServer() http.Handler {
 
 	secondaryHandler := pat.New()
 	secondaryHandler.Post(API_PREFIX+POST_PREFIX+UPLOAD_PREFIX, http.HandlerFunc(uploadHandler))
+	secondaryHandler.Post(API_PREFIX+POST_PREFIX+CREATE_DIR_PREFIX, http.HandlerFunc(createDirHandler))
+	secondaryHandler.Post(API_PREFIX+POST_PREFIX+NAVIGATION_PREFIX, http.HandlerFunc(navigationHandler))
 	secondaryHandler.Post(API_PREFIX+POST_PREFIX+LOGIN_PREFIX, http.HandlerFunc(loginHandler))
 	secondaryHandler.Post(API_PREFIX+POST_PREFIX+SIGNUP_PREFIX, http.HandlerFunc(signupHandler))
 
